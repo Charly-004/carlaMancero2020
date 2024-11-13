edff72164d21e966413fad64d0d5e78e4a0b994f
Charly-004 <charlycortes004@gmail.com>
2024-11-13 13:18:11 -0500

    Practica de laboratorio

---
+++ src/main/java/ec/edu/espoch/carlamancero2020/CarlaMancero2020.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.carlamancero2020;
+
+/**
+ *
+ * @author SO-LAB1-PC20
+ */
+public class CarlaMancero2020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
