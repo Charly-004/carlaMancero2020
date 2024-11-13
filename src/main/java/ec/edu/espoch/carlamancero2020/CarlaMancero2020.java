0067c970e05563a47ac7d521377481135387d3de
Charly-004 <charlycortes004@gmail.com>
2024-11-13 14:57:27 -0500

    Finalizacion de practica

--- src/main/java/ec/edu/espoch/carlamancero2020/CarlaMancero2020.java
+++ src/main/java/ec/edu/espoch/carlamancero2020/CarlaMancero2020.java
@@ -4,6 +4,8 @@
 
 package ec.edu.espoch.carlamancero2020;
 
+import java.util.Scanner;
+
 /**
  *
  * @author SO-LAB1-PC20
@@ -12,5 +14,47 @@ public class CarlaMancero2020 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        int [] punDiarias= new int[7];
+         
+        int puntosTotal;
+        int promedio;
+        int punAlta;
+        int punBaja;
+        
+        
+        Scanner sc = new Scanner(System.in);
+                
+        for (int i = 0; i < 7; i++) {
+            System.out.println("Ingrese la puntuacion obtenida");
+            
+           int puntuacion=sc.nextInt();
+           punDiarias[i]=puntuacion;
+           
+          
+            
+            
+            
+            
+            
+        }
+             
+ 
+               
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
